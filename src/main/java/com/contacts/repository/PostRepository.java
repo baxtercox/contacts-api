9734760ecc05a336ce7a4c9457ea85f0b2fe8a30
Baxter Cox <baxtercoxrtr@gmail.com>
2021-03-04 13:18:09 -0500

    repository interfaces

---
+++ src/main/java/com/contacts/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package com.contacts.repository;
+
+package com.technews.repository;
+
+import com.contacts.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    List<Post> findAllPostsByUserId(Integer id) throws Exception;
+
+}
---
+++ src/main/java/com/contacts/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.contacts.repository;
+
+import com.contacts.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByEmail(String email) throws Exception;
+}
